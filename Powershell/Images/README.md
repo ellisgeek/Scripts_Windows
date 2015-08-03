460d8630296804fc18fd96ecb268693e6bc85ff5
Elliott Saille <elliott@rootwerk.systems>
2015-08-03 15:57:07 -0500

    Add Whoops Script

---
+++ Powershell/Images/README.md
@@ -0,0 +1,2 @@
+
+Thanks to (/u/drmacinyasha)[https://www.reddit.com/user/drmacinyasha] for (Only You.jpg)[https://www.reddit.com/r/techsupportanimals/comments/2ulkei/my_coworker_learned_an_important_lesson_today/]
---
+++ Powershell/Whoops.ps1
@@ -0,0 +1,27 @@
+﻿function Get-ScriptDirectory {
+<#
+	.SYNOPSIS
+		Get-ScriptDirectory returns the proper location of the script.
+
+	.OUTPUTS
+		System.String
+	
+	.NOTES
+		Returns the correct path within a packaged executable.
+#>
+    [OutputType([string])]
+    if ($hostinvocation -ne $null) {
+        Split-Path $hostinvocation.MyCommand.path
+    } else {
+        Split-Path $script:MyInvocation.MyCommand.Path
+    }
+}
+
+$ScriptDirectory = Get-ScriptDirectory
+
+. "$ScriptDirectory\Helper Functions\Set-Wallpaper.ps1"
+. "$ScriptDirectory\Helper Functions\Lock-Workstation.ps1"
+
+Set-Wallpaper -Path "$ScriptDirectory\Images\Only You.jpg" -Style Center
+
+Lock-WorkStation
